6a17dbbfc7d632ba3cf68d365de504a69d3b021a
Brandon <brandontrump101@gmail.com>
2022-05-23 16:36:58 +1200

    Monday Changes

--- Week 4/Monday/app.js
+++ Week 4/Monday/app.js
@@ -11,15 +11,33 @@
 //   }
 // }
 
-let current = 0;
+// let current = 0;
 
-function changeColour() {
-  let colourArr = ["blue", "green", "orange", "purple", "red", "brown"];
+// function changeColour() {
+//   let colourArr = ["blue", "green", "orange", "purple", "red", "brown"];
 
-  if (current >= colourArr.length) {
-    current = 0;
-  }
+//   if (current >= colourArr.length) {
+//     current = 0;
+//   }
 
-  document.body.style.background = colourArr[current];
-  current++;
+//   document.body.style.background = colourArr[current];
+//   current++;
+// }
+
+// let current = 1;
+
+// function addCount() {
+//   const counter = document.getElementById("counter");
+//   counter.textContent = `Counter: ${current}`;
+//   current++;
+// }
+
+const inputElement = document.querySelector("input");
+inputElement.addEventListener("input", logValue);
+function logValue(e) {
+  console.log(e.target.value);
 }
+
+// function logValue(e) {
+//   console.log(e.target.value);
+// }
