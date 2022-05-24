fc4cc3a1a2af92de9e3606d500d62abf9a093c27
Brandon <brandontrump101@gmail.com>
2022-05-24 16:26:56 +1200

    Tuesday

--- Week 4/Monday/app.js
+++ Week 4/Monday/app.js
@@ -32,12 +32,39 @@
 //   current++;
 // }
 
-const inputElement = document.querySelector("input");
-inputElement.addEventListener("input", logValue);
-function logValue(e) {
-  console.log(e.target.value);
-}
+// const textCounter = document.querySelector("p");
+// const inputElement = document.querySelector("input");
+// inputElement.addEventListener("input", logValue);
+// function logValue(e) {
+//   textCounter.textContent = `word count: ${e.target.value.length}`;
+// }
 
 // function logValue(e) {
 //   console.log(e.target.value);
 // }
+
+// const inputNum1 = document.getElementById("inputNum1");
+// const inputNum2 = document.getElementById("inputNum2");
+// const result = document.querySelector("p");
+// const sum = document.querySelector("button");
+
+// sum.addEventListener("click", () => {
+//   const Num1 = parseInt(inputNum1.value);
+//   const Num2 = parseInt(inputNum2.value);
+
+//   const total = Num1 + Num2;
+
+//   result.textContent = `result: ${Num1} + ${Num2} = ${total}`;
+// });
+
+// function logValue(e) {
+//   textCounter.textContent = `word count: ${e.target.value.length}`;
+// }
+
+// const divideNums = (num1, num2) => num1 / num2;
+
+// console.log(divideNums(6, 2));
+
+// const checkIfEven = (num) => num % 2 === 0;
+
+// console.log(checkIfEven(6), checkIfEven(3));
