7facac160196858ddf974b9a9a1c898e8c734b4d
Brandon <brandontrump101@gmail.com>
2022-05-25 16:30:35 +1200

    Wednesday

--- Week 4/Monday/app.js
+++ Week 4/Monday/app.js
@@ -68,3 +68,66 @@
 // const checkIfEven = (num) => num % 2 === 0;
 
 // console.log(checkIfEven(6), checkIfEven(3));
+
+// const divideNums = (num1, num2 = 1) => num1 / num2;
+
+// console.log(divideNums(6, 3));
+// console.log(divideNums(10));
+
+// const fruits = ["apple", "mango", "grape", 35, "pineapple"];
+// console.log(`push array = ${fruits}`);
+
+// fruits.push("cherry");
+// console.log(`push array = ${fruits}`);
+
+// fruits.pop();
+// console.log(`pop array = ${fruits}`);
+
+// console.log(`forEach array ${fruits}`);
+
+// fruits.forEach((item) => console.log(item));
+
+// const mapArray = [0, 1, 2, 3];
+// console.log(`initial array = ${mapArray}`);
+
+// const arrayAfterMapping = mapArray.map((item) => item + 1);
+// console.log(`mapped array ${arrayAfterMapping}`);
+
+// const fruits = ["apple", "mango", "grape"];
+
+// console.log(fruits.map((fruit) => fruit.toUpperCase()));
+
+// const food = ["pizza", "pasta", "cake"];
+
+// const isFavFood = (arrayFood, food) => arrayFood.includes(food);
+
+// console.log(isFavFood(food, "pizza"));
+
+// const unfilteredArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(`unfiltered array = ${unfilteredArray}`);
+
+// const filteredArray = unfilteredArray.filter((item) => item > 4);
+// console.log(`filtered array = ${filteredArray}`);
+
+// const unfilteredArray = ["hello", "hi", "howdy", "salutations", "greetings"];
+
+// const filterArray = (array, min) => array.filter((item) => item.length > min);
+// console.log(`filtered array = ${filterArray(unfilteredArray, 6)}`);
+
+// const originalArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// originalArray.splice(3, 5);
+
+// // splice with function
+// // const spliceArray = (array) => array.splice(3, 5);
+
+// // spliceArray(originalArray);
+
+// console.log(originalArray);
+
+// const someArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(`array = ${someArray}`);
+
+// // a = previous value,  b = current value
+// const reducedValue = someArray.reduce((a, b) => a + b);
+// console.log(`reduced value = ${reducedValue}`);
