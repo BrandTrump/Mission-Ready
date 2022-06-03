f83de9d82517b407de4fc53828c671d9aa73d91a
Brandon <brandontrump101@gmail.com>
2022-06-03 16:27:59 +1200

    Friday

--- Week 5/Thursday/hello-react/src/App.js
+++ Week 5/Thursday/hello-react/src/App.js
@@ -1,10 +1,29 @@
-const message1 = "Hey you";
-const message2 = "Whats going on";
+import Footer from "./Footer";
+
+function greet(personName) {
+  return { name: `Hello ${personName}` };
+}
+
 function App() {
+  const message1 = "Hey you";
+  const message2 = "Whats going on";
   return (
     <>
-      <h1>{message1}</h1>
-      <p>{message2 + "??"}</p>
+      <>
+        <h1>{message1}</h1>
+        <p>{message2 + "??"}</p>
+      </>
+
+      <>
+        <h1>{greet("John").name}</h1>
+        <h2>How are you?</h2>
+        <h2>Today is a sunny rainy day</h2>
+      </>
+
+      <>
+        <p>This is some HTML before the footer</p>
+        <Footer></Footer>
+      </>
     </>
   );
 }
