83c172db9bcd4f765b1701d5d0c72cc0278f0050
Brandon <brandontrump101@gmail.com>
2022-06-10 14:59:11 +1200

    Test commit from test branch

--- Week 6/Tuesday/my-app/src/MainBranch.js
+++
@@ -1,7 +0,0 @@
-export default function MainBranch() {
-  return (
-    <div>
-      <h1>This is on The Main Branch</h1>
-    </div>
-  );
-}
---
+++ Week 6/Tuesday/my-app/src/TestBranch.js
@@ -0,0 +1,7 @@
+export default function TestBranch() {
+  return (
+    <div>
+      <h1>This is on The Test Branch</h1>
+    </div>
+  );
+}
