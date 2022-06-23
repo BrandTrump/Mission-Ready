9bac71c22532e4eaff8283718190a1d8d851a406
Brandon Trump <brandontrump101@gmail.com>
2022-06-23 13:19:14 +1200

    Tuesday

--- Week 8/Tuesday/axios-app/src/App.js
+++ Week 8/Tuesday/axios-app/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://reqres.in/api/users").then((res) => {
+      setUsers(res.data.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=150").then((res) => {
+      setPokemon(res.data.results);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="users">
+        {users.map((user, index) => (
+          <h3 key={index}>
+            <img src={user.avatar} alt="img"></img> {user.first_name}{" "}
+            {user.last_name}
+          </h3>
+        ))}
+      </div>
+      <div className="pokemon">
+        {pokemon.map((pokemon, index) => (
+          <h3 key={index}>
+            {index + 1}. {pokemon.name}
+          </h3>
+        ))}
+      </div>
     </div>
   );
 }
